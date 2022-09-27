38fc9bb4033dde5eaa823ddf5b94886caa21c6a8
Artyflex <denis.seo@nxt.ru>
2022-09-27 17:52:58 +0300

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useEffect, useState, useRef} from "react";
+import {Button, FormControl, Input, InputLabel, TextField} from "@mui/material";
 
 
 function App() {
@@ -19,46 +20,75 @@ function App() {
   ]);
   const [messageBody, setMessageBody] = useState({
     text: '',
-    author: ''
+    author: 'me'
   });
-
-  const firstUpdate = useRef(true);
-
 
   const ROBOT_MESSAGE = 'Привет. Я получил сообщение';
 
-  useEffect(()=> {
+  const firstUpdate = useRef(true);
+
+  useEffect(() => {
     if (firstUpdate.current) {
       firstUpdate.current = false;
       return;
     }
     if (messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot') {
       setTimeout(() => {
-        setMessageList( prevState => [...prevState, {text: ROBOT_MESSAGE, author: 'robot'}])
+        setMessageList(prevState => [...prevState, {text: ROBOT_MESSAGE, author: 'robot'}])
       }, 1500)
     }
   }, [messageList])
 
 
   return (
-    <div className="board">
+    <div className="wrapper">
+      <ChatList></ChatList>
 
-      <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
-      <div  className="messages-list">
-        {
-          messageList.map((elem, index)=> <Message text={elem.text} author={elem.author} key={index}></Message>)
-        }
+      <div className="board">
+        <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
+        <div className="messages-list">
+          {
+            messageList.map((elem, index) => <Message text={elem.text} author={elem.author} key={index}></Message>)
+          }
+        </div>
       </div>
-
     </div>
   )
 }
 
 export default App;
 
-const Message = ({author, text}) => {
+const ChatList = () => {
 
-  return(
+  const chatList = [
+    {
+      id: 1,
+      name: 'Some Name'
+    },
+    {
+      id: 2,
+      name: 'Other Name'
+    },
+    {
+      id: 3,
+      name: 'Random Name'
+    },
+  ];
+
+  return (
+    <div className="chat-list">
+      {
+        chatList.map((elem, index) => <div className="chat-list__item" key={index}>
+          {elem.name}
+        </div>)
+      }
+    </div>
+  )
+}
+
+
+const Message = ({author, text}) => {
+  return (
     <div className="message">
       <p className="message__author">
         <span>
@@ -72,7 +102,6 @@ const Message = ({author, text}) => {
       </p>
     </div>
   )
-
 }
 
 
@@ -83,7 +112,7 @@ const Form = ({data, setData, setMessage}) => {
   const submitForm = (e) => {
     e.preventDefault();
     if (text.length > 0) {
-      setMessage(prevState=> [...prevState, {text,author}])
+      setMessage(prevState => [...prevState, {text, author}])
     }
     setData(
       {
@@ -93,18 +122,26 @@ const Form = ({data, setData, setMessage}) => {
     )
   }
 
-  return(
+  // const textInput = useRef(null);
+  // useEffect(() => {
+  //   console.log('sdf!!')
+  //   textInput?.current?.focus();
+  // },[text]);
+
+  return (
     <form className="form" onSubmit={submitForm}>
-      <input placeholder='Имя' value={text} onChange={
-        (event) => setData( pervState => ({...pervState, text: event.target.value}) )
-      }/>
 
-      <input placeholder='Текст' value={author} onChange={
-        (event) => setData( pervState => ({...pervState, author: event.target.value}) )
-      }/>
+      <FormControl>
+        <TextField
+          autoFocus
+          // ref={textInput}
+          placeholder="text" id="time"
+          type="text" value={text} onChange={
+          (event) => setData(pervState => ({...pervState, text: event.target.value}))
+        }/>
+      </FormControl>
 
-      <button type="submit">Отправить</button>
+      <Button type="submit">Отправить</Button>
     </form>
   )
-
 }
